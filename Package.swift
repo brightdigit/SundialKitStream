c66405ca63bc65e46bcb693668f3a59a4cc06920
Leo Dion <leogdion@brightdigit.com>
2026-04-10 11:38:16 -0400

    Updating SundialKit

--- Package.swift
+++ Package.swift
@@ -59,7 +59,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/brightdigit/SundialKit.git", branch: "atleast-v1.0.0-beta.4")
+    .package(url: "https://github.com/brightdigit/SundialKit.git", from: "2.0.0-alpha.2")
   ],
   targets: [
     .target(
